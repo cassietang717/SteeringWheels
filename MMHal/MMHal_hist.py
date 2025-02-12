961c79d531da2200260151bf3e6986443ecf858e
Weiyi Tian <weiyitian.plus@gmail.com>
2025-02-11 23:28:06 -0600

    update MMHal hist

--- MMHal/MMHal_hist.py
+++ MMHal/MMHal_hist.py
@@ -2,6 +2,10 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
+tick_fontsize = 16
+label_fontsize = 18
+title_fontsize = 20
+
 file_path = 'output/MMHal_llava.json'
 with open(file_path, "r") as file:
     new_data = json.load(file)
@@ -16,10 +20,11 @@ score_counts_new = new_df["llama_hallucination_rating"].value_counts().sort_inde
 # Plot bar chart for the new similarity score counts
 plt.figure(figsize=(8, 6))
 plt.bar(score_counts_new.index, score_counts_new.values, color="skyblue", edgecolor="black", alpha=0.75)
-plt.xlabel("Llama Hallucination Rating")
-plt.ylabel("Count")
-plt.title("Count of Each Llama Hallucination Rating")
-plt.xticks(score_counts_new.index)
+plt.xlabel("Llama Hallucination Rating", fontsize=label_fontsize)
+plt.ylabel("Count", fontsize=label_fontsize)
+plt.title("Count of Each Llama Hallucination Rating", fontsize=title_fontsize)
+plt.xticks(score_counts_new.index, fontsize=tick_fontsize)
+plt.yticks(fontsize=tick_fontsize)
 plt.grid(axis="y", alpha=0.75)
 
 plt.savefig("figures/MMHal_llama_eval.pdf")
